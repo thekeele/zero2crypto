5cf99e4b790199c6cd27840b23d763a8eec95a96
Mark Keele <mkeele@indiana.edu>
2014-03-08 18:46:57 -0500

    Update install_package.sh

--- install_package.sh
+++ install_package.sh
@@ -5,10 +5,10 @@ sleep 2
 chmod 700 ./install/*.sh
 ./install/update_os.sh
 touch ./config/ip.txt
-./install/whois.sh  >> ./config/ip.txt
+./install/whois.sh  > ./config/ip.txt
 echo "Your IP Address is below, write this down"
 cat ./config/ip.txt
-sleep 6
+sleep 15
 ./install/get_gpu_drivers.sh
 ./install/install_gpu_drivers.sh
 ./install/install_app_sdk.sh
