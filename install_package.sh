e43f3fc151d332387b56230b52cfc3c63492dff2
Mark Keele <mkeele@indiana.edu>
2014-03-08 18:56:18 -0500

    separate into two packages

--- install_package.sh
+++ install_package.sh
@@ -11,17 +11,3 @@ cat ./config/ip.txt
 sleep 15
 ./install/get_gpu_drivers.sh
 ./install/install_gpu_drivers.sh
-./install/install_app_sdk.sh
-echo "All installations are complete"
-sleep 2
-./install/get_cgminer.sh
-echo "About to test cgminer, you should see ALL your cards"
-sleep 6
-chmod 700 ./test_install/test_cgminer.sh
-./test_install/test_cgminer.sh
-chmod 700 ./configs/get_configs.sh
-./configs/get_configs.sh
-echo "Done, mining is about to start\nWarning: Please update files in /configs for your system\n"
-sleep 6
-chmod 700 ./start_mining.sh
-./start_mining.sh
