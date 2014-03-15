b0b6f2a20f208c14e7b38d26dcbc3ae39d0e8a5a
Captain Underpants <mkeele@indiana.edu>
2014-03-15 19:52:55 -0400

    pump 270s

--- configs/start_cgminer.sh
+++ configs/start_cgminer.sh
@@ -3,4 +3,4 @@ export DISPLAY=:0
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_USE_SYNC_OBJECTS=1
 
-/opt/cgminer/cgminer --config ./waffle-r9-270.conf
+/opt/cgminer/cgminer --config ./tmb-r9-270.conf
