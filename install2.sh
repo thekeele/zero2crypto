3c2eb1b54f308f2007c444e7c21b99da21df1aaf
Mark Keele <mkeele@indiana.edu>
2014-03-25 21:19:46 -0400

    use absolute paths

--- install2.sh
+++ install2.sh
@@ -1,7 +1,7 @@
-/install/install_app_sdk.sh
+~/zero2crypto/install/install_app_sdk.sh
 echo "Installation complete"
 sleep 5
-./install/get_cgminer.sh
+~/zero2crypto/install/get_cgminer.sh
 echo "About to test cgminer installation"
-./test_install/test_cgminer.sh
+~/zero2crypto/test_install/test_cgminer.sh
 echo "You should see all your cards above"
