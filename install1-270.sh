c387e2b89a2246528dbd268366193a2f972d8b92
Mark Keele <mkeele@indiana.edu>
2014-03-25 23:47:38 -0400

    whoops

--- install1-270.sh
+++ install1-270.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 #Zero to Crypto on Xubuntu
-echo "Zero to Crypto on Xubuntu\n\n"
+echo "Zero to Crypto on Xubuntu"
 sleep 5
 ~/zero2crypto/install/update_os.sh
-echo "Your IP Address is below, write this down\n\n"
+echo "Your IP Address is below, write this down"
 echo  `~/zero2crypto/install/whois.sh`
 sleep 15
 ~/zero2crypto/install/get_270_driver.sh
