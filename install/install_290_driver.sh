e8efd21f9a16145f833be56df4d7f71ac83407b5
Mark Keele <mkeele@indiana.edu>
2014-03-25 23:52:46 -0400

    290 driver install instructions

--- install/install_290_driver.sh
+++ install/install_290_driver.sh
@@ -6,13 +6,21 @@ cd ~/Downloads/driver
 unzip driver.zip
 
 #change long name of driver to driver.run. 
-mv amd-catalyst-13.11-betaV9.95-linux-x86.x86_64.run driver.run
+mv amd-driver-installer-13.35.1005-x86.x86_64.run driver.run
 
 #set correct permissions on driver.run
 chmod a+x driver.run
 
 #install the driver.  Go through the prompts, select recommended.  Do not stray off the path!
 sudo ./driver.run
+
+#amd dependencies
+sudo dpkg -i fglrx*.deb
+sudo apt-get -f install
+sudo dpkg -i fglrx*.deb
+
+#list adapters
+sudo aticonfig --lsa
 
 #initialize adapters
 sudo aticonfig --adapter=all --initial
