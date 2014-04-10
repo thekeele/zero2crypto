583f5e971c34b79bad2cc53225c170d66732a2fc
Mark Keele <mkeele@indiana.edu>
2014-04-09 22:40:30 -0400

    driver problems

--- install/get_290_driver.sh
+++ install/get_290_driver.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 cd ~/Downloads
 
-wget http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
+curl -o amd-catalyst-13.12-linux-x86.x86_64.run https://googledrive.com/host/0B0YyMyCIj-ORSlEyLWsxZnpYTzQ
 curl -o AMD-APP-SDK-v2.9-lnx64.tgz https://googledrive.com/host/0B0YyMyCIj-ORaUU5THRoX0tnSWM
 curl -o ADL_SDK_6.0.zip https://googledrive.com/host/0B0YyMyCIj-ORb3RISWhWSWtKYjg
 
 mkdir driver app adl
 
-mv amd-catalyst-13.12-linux-x86.x86_64.zip ./driver/
+mv amd-catalyst-13.12-linux-x86.x86_64.run ./driver/
 mv AMD-APP-SDK-v2.9-lnx64.tgz ./app/
 mv ADL_SDK_6.0.zip ./adl/
