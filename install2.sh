237edafb6a5b445b4fb745d6ac0979e8cafa442c
Mark Keele <mkeele@indiana.edu>
2014-04-09 20:00:48 -0400

    restructure

--- install2.sh
+++
@@ -1,7 +0,0 @@
-~/zero2crypto/install/install_app_sdk.sh
-echo "Installation complete"
-sleep 5
-~/zero2crypto/install/get_cgminer.sh
-echo "About to test cgminer installation"
-~/zero2crypto/test_install/test_cgminer.sh
-echo "You should see all your cards above"
