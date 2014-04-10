baa0b24bd9dbbf8c9a965c99f367c60e34d77f35
Mark Keele <mkeele@indiana.edu>
2014-04-09 23:09:16 -0400

    update cgminer

--- test/test_cgminer.sh
+++ test/test_cgminer.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
-export DISPLAY=:0 
-export GPU_MAX_ALLOC_PERCENT=100 
+export DISPLAY=:0
 export GPU_USE_SYNC_OBJECTS=1
-cd ~/Downloads/cgminer/cgminer-3.1.1
+export GPU_MAX_ALLOC_PERCENT=100
+export GPU_MAX_HEAP_SIZE=100
+cd ~/Downloaed/cgminer/cgminer-3.7.2
 ./cgminer -n
