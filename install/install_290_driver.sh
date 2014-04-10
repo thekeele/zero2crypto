365bebcedeb0683dc95c14e2625ce6099b59b60f
Mark Keele <mkeele@indiana.edu>
2014-04-09 22:13:03 -0400

    working

--- install/install_290_driver.sh
+++ install/install_290_driver.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
-cd ~/Downloads/driver
+cd ~/Downloads/driver/
 
 unzip amd-catalyst-13.12-linux-x86.x86_64.zip
-
-mv amd-driver-installer-13.35.1005-x86.x86_64.run driver.run
 
 chmod a+x driver.run
 
