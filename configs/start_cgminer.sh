7789db6864ff893d28220c3505b1ae1a573ca2f9
Mark Keele <mkeele@indiana.edu>
2014-04-09 23:26:15 -0400

    no debug

--- configs/start_cgminer.sh
+++ configs/start_cgminer.sh
@@ -3,4 +3,4 @@ export DISPLAY=:0
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_USE_SYNC_OBJECTS=1
 
-~/Downloads/cgminer/cgminer-3.7.2/cgminer -T --config ~/zero2crypto/configs/tmb-r9-290.conf
+~/Downloads/cgminer/cgminer-3.7.2/cgminer --config ~/zero2crypto/configs/tmb-r9-290.conf
