caa913c11d903bc8088894ad38613b878df9ebab
Mark Keele <mkeele@indiana.edu>
2014-04-09 20:05:38 -0400

    keep it simple

--- install/update_os.sh
+++ install/update_os.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
-echo "Let's get the latest packages for the OS and a few extra\n\n"
 sudo apt-get update 
 sudo apt-get upgrade
 sudo apt-get install openssh-server screen build-essential autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config libudev-dev curl
