aa1bf7cafede3339645428766dc3b2ba513392fc
Mark Keele <mkeele@indiana.edu>
2014-04-09 23:10:42 -0400

    typo

--- test/test_cgminer.sh
+++ test/test_cgminer.sh
@@ -3,5 +3,5 @@ export DISPLAY=:0
 export GPU_USE_SYNC_OBJECTS=1
 export GPU_MAX_ALLOC_PERCENT=100
 export GPU_MAX_HEAP_SIZE=100
-cd ~/Downloaed/cgminer/cgminer-3.7.2
+cd ~/Downloads/cgminer/cgminer-3.7.2
 ./cgminer -n
