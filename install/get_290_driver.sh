0f9fded67b24d24686b14cb9b6528d7b7967690c
Mark Keele <mkeele@indiana.edu>
2014-04-09 21:44:42 -0400

    correct urls

--- install/get_290_driver.sh
+++ install/get_290_driver.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 cd ~/Downloads
 
-wget http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
-wget https://drive.google.com/uc?export=download&id=0B0YyMyCIj-ORaUU5THRoX0tnSWM
-wget https://drive.google.com/uc?export=download&id=0B0YyMyCIj-ORb3RISWhWSWtKYjg
+curl -O http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
+curl -o AMD-APP-SDK-v2.9-lnx64.tgz https://googledrive.com/host/0B0YyMyCIj-ORaUU5THRoX0tnSWM
+curl -o ADL_SDK_6.0.zip https://googledrive.com/host/0B0YyMyCIj-ORb3RISWhWSWtKYjg
 
 mkdir driver app adl
 
