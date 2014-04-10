5f7aa37328ce3e02d00f971483653f5ae11e270c
Mark Keele <mkeele@indiana.edu>
2014-04-09 21:20:52 -0400

    new sources

--- install/get_290_driver.sh
+++ install/get_290_driver.sh
@@ -1,17 +1,12 @@
 #!/bin/bash
-# Change directory to /home/$user/Downloads
 cd ~/Downloads
 
-# Latest Driver that supports r9 290
-curl -o driver.zip http://www2.ati.com/drivers/beta/Linux_AMD_Catalyst_14.3_Beta_V1.0_B22_March12_2014.zip
+wget http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
+wget https://drive.google.com/uc?export=download&id=0B0YyMyCIj-ORaUU5THRoX0tnSWM
+wget https://drive.google.com/uc?export=download&id=0B0YyMyCIj-ORb3RISWhWSWtKYjg
 
-# Download AMD-APP-SDK-v2.9-lnx64.tgz from TruePPS server (sorry, can't figure out a direct download at the time of writing)
-curl -o amd-app-sdk.tgz http://blog.truepps.com/downloads/AMD/AMD-APP-SDK-v2.9-lnx64.tgz
+mkdir driver app adl
 
-#Download ADL_SDK_6.0.zip from TruePPS server (same deal)
-curl -o ADL_SDK.zip http://blog.truepps.com/downloads/AMD/ADL_SDK_6.0.zip
-
-mkdir AMD-APP-SDK driver ADL_SDK
-mv amd-app-sdk.tgz AMD-APP-SDK
-mv driver.zip driver
-mv ADL_SDK.zip ADL_SDK
+mv amd-catalyst-13.12-linux-x86.x86_64.zip ./driver/
+mv AMD-APP-SDK-v2.9-lnx64.tgz ./app/
+mv ADL_SDK_6.0.zip ./adl/
