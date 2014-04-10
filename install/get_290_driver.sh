409303280d8d0609c9089dad4b23cb17d5cf8afa
Mark Keele <mkeele@indiana.edu>
2014-04-09 21:47:27 -0400

    rename all

--- install/get_290_driver.sh
+++ install/get_290_driver.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 cd ~/Downloads
 
-curl -O http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
+curl -o amd-catalyst-13.12-linux-x86.x86_64.zip http://www2.ati.com/drivers/linux/amd-catalyst-13.12-linux-x86.x86_64.zip
 curl -o AMD-APP-SDK-v2.9-lnx64.tgz https://googledrive.com/host/0B0YyMyCIj-ORaUU5THRoX0tnSWM
 curl -o ADL_SDK_6.0.zip https://googledrive.com/host/0B0YyMyCIj-ORb3RISWhWSWtKYjg
 
