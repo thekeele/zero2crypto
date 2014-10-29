1c20de4cb7bd43845a514591f370839c6eb90d61
Mark Keele <m.keele88@gmail.com>
2014-10-29 18:44:32 -0400

    simplify

--- install/install_driver.sh
+++ install/install_driver.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-cd ~/Downloads/AMD-APP-SDK/
-tar xvf AMD-APP-SDK-v2.9-lnx64.tgz
-sudo ./Install-AMD-APP.sh
-
+cd ~/Downloads
+tar xvf AMD-APP-SDK*
+rm *.bz2
+sudo ./AMD-APP-SDK*
 sudo reboot
