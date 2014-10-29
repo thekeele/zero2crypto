0b81609c37d42c17deaa4a99ba43395d975d1485
Mark Keele <m.keele88@gmail.com>
2014-10-29 18:48:45 -0400

    unnecessary

--- install/get_drivers.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-cd ~/Downloads
-
-curl -o AMD-APP-SDK-v2.9-lnx64.tgz https://googledrive.com/host/0B0YyMyCIj-ORaUU5THRoX0tnSWM
-curl -o ADL_SDK_6.0.zip https://googledrive.com/host/0B0YyMyCIj-ORb3RISWhWSWtKYjg
