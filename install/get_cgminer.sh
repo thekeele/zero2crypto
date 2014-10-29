820e982fbd339633493cdb610473b9de2439c5c0
Mark Keele <m.keele88@gmail.com>
2014-10-29 18:56:39 -0400

    simplify

--- install/get_cgminer.sh
+++ install/get_cgminer.sh
@@ -4,11 +4,10 @@ mkdir cgminer ; cd cgminer
 wget http://ck.kolivas.org/apps/cgminer/old/3.7/cgminer-3.7.2.tar.bz2
 bzip2 -d cgminer-3.7.2.tar.bz2 ; tar -xf cgminer-3.7.2.tar
 
-cd ~/Downloads/adl
-unzip ADL_SDK_6.0.zip
-cp include/* /home/rakthamac/Downloads/cgminer/cgminer-3.7.2/ADL_SDK/
+cd ~/Downloads
+unzip ADL_SDK*
+cp include/* ~/Downloads/cgminer/cgminer-3.7.2/ADL_SDK/
 
-cd ~/Downloads/cgminer/cgminer-3.7.2
-make clean
+cd ~/Downloads/cgminer/cgminer-3.7.2/
 CFLAGS="-O2 -Wall -march=native -I /opt/AMDAPP/include/" LDFLAGS="-L/opt/AMDAPP/lib/x86" ./configure --enable-opencl --enable-scrypt
 make
