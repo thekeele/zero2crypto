de4edd433ba9813ab939d9cfbe4f0d63623d084b
Mark Keele <m.keele88@gmail.com>
2014-10-29 18:22:28 -0400

    additional dependencies

--- install/update_os.sh
+++ install/update_os.sh
@@ -3,5 +3,6 @@ sudo apt-get update
 sudo apt-get upgrade
 sudo apt-get install openssh-server screen build-essential autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config libudev-dev curl
 sudo apt-get -y install build-essential unzip cmake automake libncurses5-dev autoconf libcurl4-openssl-dev pkg-config automake yasm
+sudo apt-get install dh-modaliases execstack debhelper dkms lib32gcc1
 sudo apt-get install vim
 sudo apt-get install ssh
