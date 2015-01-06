a4aa3853234d5c6376d333970b9ffb222f07fdd4
Mark Keele <m.keele88@gmail.com>
2015-01-06 18:46:30 -0500

    update os

--- install/update_os.sh
+++ install/update_os.sh
@@ -4,7 +4,9 @@ sudo apt-get upgrade
 sudo apt-get install openssh-server screen build-essential autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config libudev-dev curl
 sudo apt-get -y install build-essential unzip cmake automake libncurses5-dev autoconf libcurl4-openssl-dev pkg-config automake yasm
 sudo apt-get install dh-modaliases execstack debhelper dkms lib32gcc1
-sudo apt-get install python-dev python-twisted python-argparse
+sudo apt-get install python-dev python-twisted python-argparse python-setuptools
 sudo apt-get install libffi-dev
 sudo apt-get install vim
 sudo apt-get install ssh
+sudo easy_install pip
+sudo pip install service_identity
