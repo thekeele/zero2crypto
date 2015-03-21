86d0810dcfe1b2e847ea46b47ec3e1b3478635e9
Mark Keele <m.keele88@gmail.com>
2015-03-21 18:05:13 -0400

    clean up redundancies

--- install/update_os.sh
+++ install/update_os.sh
@@ -1,12 +1,9 @@
 #!/bin/bash
 sudo apt-get update 
 sudo apt-get upgrade
-sudo apt-get install openssh-server screen build-essential autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config libudev-dev curl
-sudo apt-get -y install build-essential unzip cmake automake libncurses5-dev autoconf libcurl4-openssl-dev pkg-config automake yasm
-sudo apt-get install dh-modaliases execstack debhelper dkms lib32gcc1
+sudo apt-get install screen build-essential autoconf libtool libcurl4-openssl-dev libncurses5-dev pkg-config libudev-dev unzip cmake automake yas
+sudo apt-get install dh-modaliases execstack debhelper dkms lib32gcc1 libffi-dev
 sudo apt-get install python-dev python-twisted python-argparse python-setuptools
-sudo apt-get install libffi-dev
-sudo apt-get install vim
-sudo apt-get install ssh
+sudo apt-get install vim ssh
 sudo easy_install pip
 sudo pip install service_identity
